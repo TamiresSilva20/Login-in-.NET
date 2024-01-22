0abc1fc012460c3463469f98d07266912a7a6d0e
Tamires Silva <tamires.marlene15@gmail.com>
2024-01-22 20:39:45 -0300

    Adicionar arquivos de projeto.

---
+++ Login.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace logintcc
+{
+    public partial class Login1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+        {
+            
+                // Simulando um novo login 
+                string email = txtEmail.Text;
+            string senha = txtSenha.Text;
+            string conf_Senha = txtConfSenha.Text;
+            if (senha != conf_Senha)
+            {
+                lblMensagem.Text = "A senha e a confirmação de senha não coincidem.";
+                return;
+            }
+
+            using (MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=tcc0124;port=3306;password=Root;"))
+           {
+                    conn.Open();
+
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+
+                        // Verificar se o email e a senha correspondem a um usuário na tabela Usuario
+                        cmd.CommandText = "SELECT COUNT(*) FROM Usuario WHERE email = @email AND senha = @senha AND conf_senha=@conf_senha";
+                        cmd.Parameters.AddWithValue("@email", email);
+                        cmd.Parameters.AddWithValue("@senha", senha);
+                    cmd.Parameters.AddWithValue("@conf_senha", conf_Senha);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        if (count > 0)
+                        {
+                        // O email e a senha correspondem a um usuário existente
+                        // Agora, você pode inserir os dados na tabela login
+                        ///cmd.Parameters.Clear();
+                      ///  cmd.CommandText = "INSERT INTO login (idemail) VALUES (@loginEmail)";
+                      ///  cmd.Parameters.AddWithValue("@loginEmail", email);  // Usar um novo nome de parâmetro para evitar conflitos
+
+                        cmd.ExecuteNonQuery();
+
+
+                        Response.Redirect("http://criaecoitaqua.com.br/");
+                    }
+                        else
+                        {
+                            // As credenciais não correspondem a nenhum usuário
+                            lblMensagem.Text = "Credenciais inválidas. Verifique seu email e senha.";
+                        }
+                    }
+                }
+            }
+
+        }
+    }
---
+++ Sign_up.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+
+namespace logintcc
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        protected void BtnCadastrar_Click(object sender, EventArgs e)
+        {
+
+            using (MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=tcc0124;port=3306;password=Root;"))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "Select * from Usuario where email = @email  ";
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+
+                    using (MySqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.HasRows)
+                        {
+                            lblMensagem.Text = "Email já Cadastrado";
+                            dr.Close();
+                        }
+
+                        else
+                        {
+                            dr.Close();
+                            cmd.Parameters.Clear();
+                            cmd.CommandText = "insert into Usuario(email,nome,telefone,idestado,senha,conf_senha,concorda) values (@email, @nome, @telefone, @idestado, @senha,@conf_senha,@concorda)";
+
+                            cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                            cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                            cmd.Parameters.AddWithValue("@telefone", txtTele.Text);
+                            cmd.Parameters.AddWithValue("@idestado", ddlEstado.SelectedValue);
+                            cmd.Parameters.AddWithValue("@senha", txtSenha.Text);
+                            cmd.Parameters.AddWithValue("@conf_senha", txtConfSenha.Text);
+                            cmd.Parameters.AddWithValue("@concorda", ckbConcorda.Checked);
+
+                            cmd.ExecuteNonQuery();
+                            lblMensagem.Text = " Registro incluido com sucesso";
+                        }
+                        conn.Close();
+
+                        conn.Dispose();
+
+                    }
+                }
+            }
+        }
+
+    }
+    }
+
